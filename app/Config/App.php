2964c5f571601931c16b9fa011905c63a252b114
SidikR <rahmad.119140096@student.itera.ac.id>
2023-07-17 06:31:34 +0700

    update

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://baim.iterahero.com/';
+    public string $baseURL = 'http://localhost:8080/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
